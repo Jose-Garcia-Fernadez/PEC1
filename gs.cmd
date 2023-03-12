dfbbe9d9f9464fe0d65aa1bd801d3bc61bf3d9fc
Jose-Garcia-Fernandez <fuentedelarosa@gmail.com>
2023-03-12 11:28:31 +0100

    Prueba

--- gs.cmd
+++ gs.cmd
@@ -3,9 +3,16 @@ rem git commit -m "comentario"
 rem git remote add origin https://github.com/Jose-Garcia-Fernadez/DIRECTORIO-PUBLICO.git
 rem git push -u origin master
 rem https://nvdaes.github.io/GitHub-CLI/#:~:text=GitHub%20CLI%20es%20una%20herramienta%20de%20l%C3%ADnea%20de,desde%20la%20l%C3%ADnea%20de%20comandos%20de%20tu%20computadora.
-echo add
+
+
+
+set c=%1%
+if "%1%"=="" ( set c="Prueba")
+
 git add .
-git commit -m %1
+git commit -m %c%
+rem la primera vez
+rem git push --set-upstream https://github.com/Jose-Garcia-Fernadez/PEC1.git master
 git push https://github.com/Jose-Garcia-Fernadez/PEC-1.git
-rem envia a la carpeta PEC-1
-echo %1
+
+echo %c%
